b669b325075b707e512722dac947b155451f4221
Yanick Witschi <yanick.witschi@terminal42.ch>
2025-07-21 18:06:45 +0200

    Remove perPage from palette

--- contao/dca/tl_content.php
+++ contao/dca/tl_content.php
@@ -1,6 +1,6 @@
 <?php
 
-$GLOBALS['TL_DCA']['tl_content']['palettes']['seal_search'] = '{type_legend},type,headline;{config_legend},search_index,perPage;{template_legend:collapsed},customTpl;{protected_legend:collapsed},protected;{expert_legend:collapsed},cssID;{invisible_legend:collapsed},invisible,start,stop';
+$GLOBALS['TL_DCA']['tl_content']['palettes']['seal_search'] = '{type_legend},type,headline;{config_legend},search_index;{template_legend:collapsed},customTpl;{protected_legend:collapsed},protected;{expert_legend:collapsed},cssID;{invisible_legend:collapsed},invisible,start,stop';
 
 $GLOBALS['TL_DCA']['tl_content']['fields']['search_index'] = [
     'inputType' => 'select',
